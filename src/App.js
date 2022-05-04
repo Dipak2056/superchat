bd16e6746f62a1e6afb46bf0a044bda684c93ffb
Dipak 2056 <paudelsantosh508@gmail.com>
2022-05-04 18:28:38 +1000

    done according to the firebase v 9.0

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import "./App.css";
-import firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/auth";
+import firebase from "firebase/compat/app"; //because of version change the module location were also changed
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
 
 import { useAuthState } from "react-firebase-hooks/auth";
-import { useCollectionData } from "react-firebase-hooks";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { ChatRoom } from "./components/ChatRoom";
+import { SignIn } from "./components/SignIn";
 
 firebase.initializeApp({
-  //your config
+  //my config
+  apiKey: "AIzaSyDztDOzroiG5S_22YQkdqyUthPCnt8ad0A",
+  authDomain: "superchat-3ffbe.firebaseapp.com",
+  projectId: "superchat-3ffbe",
+  storageBucket: "superchat-3ffbe.appspot.com",
+  messagingSenderId: "1021874910622",
+  appId: "1:1021874910622:web:79fe819992939ea927928f",
+  measurementId: "G-FSF4JBF7CJ",
 });
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
 function App() {
+  const [user] = useAuthState(auth); //to check if the user is logged in or not
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header></header>
+      <section>{user ? <ChatRoom /> : <SignIn />}</section>
     </div>
   );
 }
